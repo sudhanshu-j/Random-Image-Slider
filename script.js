ba4e98b3226600c32bdba54f61a6208694449764
Sudhanshu Shekhar Jha <156537716+sudhanshu-j@users.noreply.github.com>
2024-03-03 22:30:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+next.addEventListener("click", function () {
+  let items = document.querySelectorAll(".item");
+  document.querySelector(".slide").appendChild(items[0]);
+});
+
+prev.addEventListener("click", function () {
+  let items = document.querySelectorAll(".item");
+  document.querySelector(".slide").prepend(items[items.length - 1]);
+});
